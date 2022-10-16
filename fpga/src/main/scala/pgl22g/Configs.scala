04ebec90a0a65278e8755dcfe1d2f1a5c8671f5d
Chiro Liang <Chiro2001@163.com>
2022-10-16 13:59:53 +0800

    use Arty copy

--- fpga/src/main/scala/pgl22g/Configs.scala
+++ fpga/src/main/scala/pgl22g/Configs.scala
@@ -1,77 +1,41 @@
+// See LICENSE for license details.
 package chipyard.fpga.pgl22g
 
-import sys.process._
-import freechips.rocketchip.config.{Config, Parameters}
-import freechips.rocketchip.subsystem.{ControlBusKey, ExtMem, PeripheryBusKey, SystemBusKey}
-import freechips.rocketchip.devices.debug.{DebugModuleKey, ExportDebug, JTAG}
-import freechips.rocketchip.devices.tilelink.{BootROMLocated, DevNullParams}
-import freechips.rocketchip.diplomacy.{AddressSet, DTSModel, DTSTimebase, RegionType}
-import freechips.rocketchip.tile.XLen
-import sifive.blocks.devices.spi.{PeripherySPIKey, SPIParams}
-import sifive.blocks.devices.uart.{PeripheryUARTKey, UARTParams}
-import sifive.fpgashells.shell.DesignKey
-import sifive.fpgashells.shell.pango.pgl22gshell.PGL22GDDRSize
-// import sifive.fpgashells.shell.pango.{PGL22GShellPMOD, PGL22GDDRSize}
+import freechips.rocketchip.config._
+import freechips.rocketchip.subsystem._
+import freechips.rocketchip.devices.debug._
+import freechips.rocketchip.devices.tilelink._
+import freechips.rocketchip.diplomacy.{DTSModel, DTSTimebase}
+import freechips.rocketchip.system._
+import freechips.rocketchip.tile._
+
+import sifive.blocks.devices.uart._
 
 import testchipip.{SerialTLKey}
 
-import chipyard.{BuildSystem, ExtTLMem, DefaultClockFrequencyKey}
+import chipyard.{BuildSystem}
 
 class WithDefaultPeripherals extends Config((site, here, up) => {
-  case PeripheryUARTKey => List(UARTParams(address = BigInt(0x64000000L)))
-  // case PeripherySPIKey => List(SPIParams(rAddress = BigInt(0x64001000L)))
-  // case PGL22GShellPMOD => "SDIO"
-})
-
-class WithSystemModifications extends Config((site, here, up) => {
-  case DTSTimebase => BigInt((1e6).toLong)
-  case BootROMLocated(x) => up(BootROMLocated(x), site).map { p =>
-    // invoke makefile for sdboot
-    val freqMHz = (site(DefaultClockFrequencyKey) * 1e6).toLong
-    val make = s"make -C fpga/src/main/resources/pgl22g/sdboot PBUS_CLK=${freqMHz} bin"
-    require (make.! == 0, "Failed to build bootrom")
-    p.copy(hang = 0x10000, contentFileName = s"./fpga/src/main/resources/pgl22g/sdboot/build/sdboot.bin")
-  }
-  case ExtMem => up(ExtMem, site).map(x => x.copy(master = x.master.copy(size = site(PGL22GDDRSize)))) // set extmem to DDR size
+  case PeripheryUARTKey => List(
+    UARTParams(address = 0x10013000))
+  case DTSTimebase => BigInt(32768)
+  case JtagDTMKey => new JtagDTMConfig (
+    idcodeVersion = 2,
+    idcodePartNum = 0x000,
+    idcodeManufId = 0x489,
+    debugIdleCycles = 5)
   case SerialTLKey => None // remove serialized tl port
 })
-
 // DOC include start: AbstractPGL22G and Rocket
 class WithPGL22GTweaks extends Config(
-  // harness binders
-  new WithUART ++
-  // new WithSPISDCard ++
-  new WithDDRMem ++
-  // io binders
-  new WithUARTIOPassthrough ++
-  // new WithSPIIOPassthrough ++
-  // new WithTLIOPassthrough ++
-  // other configuration
+  new WithPGL22GJTAGHarnessBinder ++
+  new WithPGL22GUARTHarnessBinder ++
+  new WithPGL22GResetHarnessBinder ++
+  new WithDebugResetPassthrough ++
   new WithDefaultPeripherals ++
-  new chipyard.config.WithTLBackingMemory ++ // use TL backing memory
-  // new WithSystemModifications ++ // setup busses, use sdboot bootrom, setup ext. mem. size
-  new chipyard.config.WithNoDebug ++ // remove debug module
-  new freechips.rocketchip.subsystem.WithoutTLMonitors ++
-  new freechips.rocketchip.subsystem.WithNMemoryChannels(1) ++
-  new WithFPGAFrequency(100) // default 100MHz freq
-)
+  new freechips.rocketchip.subsystem.WithNBreakpoints(2))
 
-class RocketPGL22GConfig extends Config(
+class TinyRocketPGL22GConfig extends Config(
   new WithPGL22GTweaks ++
-  new chipyard.RocketConfig)
+  new chipyard.TinyRocketConfig)
 // DOC include end: AbstractPGL22G and Rocket
-
-class BoomPGL22GConfig extends Config(
-  new WithFPGAFrequency(50) ++
-  new WithPGL22GTweaks ++
-  new chipyard.MegaBoomConfig)
-
-class WithFPGAFrequency(fMHz: Double) extends Config(
-  new chipyard.config.WithPeripheryBusFrequency(fMHz) ++ // assumes using PBUS as default freq.
-  new chipyard.config.WithMemoryBusFrequency(fMHz)
-)
-
-class WithFPGAFreq25MHz extends WithFPGAFrequency(25)
-class WithFPGAFreq50MHz extends WithFPGAFrequency(50)
-class WithFPGAFreq75MHz extends WithFPGAFrequency(75)
-class WithFPGAFreq100MHz extends WithFPGAFrequency(100)
--- fpga/src/main/scala/pgl22g/HarnessBinders.scala
+++ fpga/src/main/scala/pgl22g/HarnessBinders.scala
@@ -1,45 +1,79 @@
 package chipyard.fpga.pgl22g
 
 import chisel3._
-import chisel3.experimental.{BaseModule}
 
-import freechips.rocketchip.util.{HeterogeneousBag}
-import freechips.rocketchip.tilelink.{TLBundle}
+import freechips.rocketchip.devices.debug._
+import freechips.rocketchip.jtag.{JTAGIO}
+import freechips.rocketchip.subsystem._
 
-import sifive.blocks.devices.uart.{HasPeripheryUARTModuleImp, UARTPortIO}
-import sifive.blocks.devices.spi.{HasPeripherySPI, SPIPortIO}
+import sifive.blocks.devices.uart._
+import sifive.blocks.devices.jtag._
+import sifive.blocks.devices.pinctrl._
 
-import chipyard.{HasHarnessSignalReferences, CanHaveMasterTLMemPort}
-import chipyard.harness.{OverrideHarnessBinder}
+import sifive.fpgashells.ip.pango.{IBUFG, IOBUF, PULLUP, PowerOnResetFPGAOnly}
 
-/*** UART ***/
-class WithUART extends OverrideHarnessBinder({
-  (system: HasPeripheryUARTModuleImp, th: BaseModule with HasHarnessSignalReferences, ports: Seq[UARTPortIO]) => {
-    th match { case pgl22g: PGL22GFPGATestHarnessImp => {
-      pgl22g.pgl22gOuter.io_uart_bb.bundle <> ports.head
-    } }
+import chipyard.harness.{ComposeHarnessBinder, OverrideHarnessBinder}
+import chipyard.iobinders.JTAGChipIO
+
+class WithPGL22GResetHarnessBinder extends ComposeHarnessBinder({
+  (system: HasPeripheryDebugModuleImp, th: PGL22GFPGATestHarness, ports: Seq[Bool]) => {
+    require(ports.size == 2)
+
+    withClockAndReset(th.clock_32MHz, th.ck_rst) {
+      // Debug module reset
+      th.dut_ndreset := ports(0)
+
+      // JTAG reset
+      ports(1) := PowerOnResetFPGAOnly(th.clock_32MHz)
+    }
   }
 })
-//
-// /*** SPI ***/
-// class WithSPISDCard extends OverrideHarnessBinder({
-//   (system: HasPeripherySPI, th: BaseModule with HasHarnessSignalReferences, ports: Seq[SPIPortIO]) => {
-//     th match { case pgl22g: PGL22GFPGATestHarnessImp => {
-//       pgl22g.pgl22gOuter.io_spi_bb.bundle <> ports.head
-//     } }
-//   }
-// })
 
-/*** Experimental DDR ***/
-class WithDDRMem extends OverrideHarnessBinder({
-  (system: CanHaveMasterTLMemPort, th: BaseModule with HasHarnessSignalReferences, ports: Seq[HeterogeneousBag[TLBundle]]) => {
-    th match { case pgl22g: PGL22GFPGATestHarnessImp => {
-      require(ports.size == 1)
+class WithPGL22GJTAGHarnessBinder extends OverrideHarnessBinder({
+  (system: HasPeripheryDebug, th: PGL22GFPGATestHarness, ports: Seq[Data]) => {
+    ports.map {
+      case j: JTAGChipIO =>
+        withClockAndReset(th.buildtopClock, th.hReset) {
+          val jtag_wire = Wire(new JTAGIO)
+          jtag_wire.TDO.data := j.TDO
+          jtag_wire.TDO.driven := true.B
+          j.TCK := jtag_wire.TCK
+          j.TMS := jtag_wire.TMS
+          j.TDI := jtag_wire.TDI
 
-      val bundles = pgl22g.pgl22gOuter.ddrClient.out.map(_._1)
-      val ddrClientBundle = Wire(new HeterogeneousBag(bundles.map(_.cloneType)))
-      bundles.zip(ddrClientBundle).foreach { case (bundle, io) => bundle <> io }
-      ddrClientBundle <> ports.head
-    } }
+          val io_jtag = Wire(new JTAGPins(() => new BasePin(), false)).suggestName("jtag")
+
+          JTAGPinsFromPort(io_jtag, jtag_wire)
+
+          io_jtag.TCK.i.ival := IBUFG(IOBUF(th.jd_2).asClock).asBool
+
+          IOBUF(th.jd_5, io_jtag.TMS)
+          PULLUP(th.jd_5)
+
+          IOBUF(th.jd_4, io_jtag.TDI)
+          PULLUP(th.jd_4)
+
+          IOBUF(th.jd_0, io_jtag.TDO)
+
+          // mimic putting a pullup on this line (part of reset vote)
+          th.SRST_n := IOBUF(th.jd_6)
+          PULLUP(th.jd_6)
+
+          // ignore the po input
+          io_jtag.TCK.i.po.map(_ := DontCare)
+          io_jtag.TDI.i.po.map(_ := DontCare)
+          io_jtag.TMS.i.po.map(_ := DontCare)
+          io_jtag.TDO.i.po.map(_ := DontCare)
+        }
+    }
+  }
+})
+
+class WithPGL22GUARTHarnessBinder extends OverrideHarnessBinder({
+  (system: HasPeripheryUARTModuleImp, th: PGL22GFPGATestHarness, ports: Seq[UARTPortIO]) => {
+    withClockAndReset(th.clock_32MHz, th.ck_rst) {
+      IOBUF(th.uart_rxd_out,  ports.head.txd)
+      ports.head.rxd := IOBUF(th.uart_txd_in)
+    }
   }
 })
