538e91c2ddf397d3504cd05336eb9091a0712295
Chiro Liang <Chiro2001@163.com>
2022-10-16 22:42:44 +0800

    applying Memories

--- fpga/src/main/scala/pgl22g/HarnessBinders.scala
+++ fpga/src/main/scala/pgl22g/HarnessBinders.scala
@@ -10,7 +10,7 @@ import sifive.blocks.devices.uart._
 import sifive.blocks.devices.jtag._
 import sifive.blocks.devices.pinctrl._
 
-import sifive.fpgashells.ip.pango.{IBUFG, IOBUF, PULLUP, PowerOnResetFPGAOnly}
+import sifive.fpgashells.ip.pango.{IBUFG, GTP_IOBUF, PULLUP, PowerOnResetFPGAOnly}
 
 import chipyard.harness.{ComposeHarnessBinder, OverrideHarnessBinder}
 import chipyard.iobinders.JTAGChipIO
@@ -19,12 +19,12 @@ class WithPGL22GResetHarnessBinder extends ComposeHarnessBinder({
   (system: HasPeripheryDebugModuleImp, th: PGL22GFPGATestHarness, ports: Seq[Bool]) => {
     require(ports.size == 2)
 
-    withClockAndReset(th.clock_32MHz, th.ck_rst) {
-      // Debug module reset
-      th.dut_ndreset := ports(0)
-
-      // JTAG reset
-      ports(1) := PowerOnResetFPGAOnly(th.clock_32MHz)
+    withClockAndReset(th.clock_32MHz, th.resetP) {
+      // // Debug module reset
+      // th.dut_ndreset := ports(0)
+      //
+      // // JTAG reset
+      // ports(1) := PowerOnResetFPGAOnly(th.clock_32MHz)
     }
   }
 })
@@ -34,36 +34,36 @@ class WithPGL22GJTAGHarnessBinder extends OverrideHarnessBinder({
     ports.map {
       case j: JTAGChipIO =>
         withClockAndReset(th.buildtopClock, th.hReset) {
-          val jtag_wire = Wire(new JTAGIO)
-          jtag_wire.TDO.data := j.TDO
-          jtag_wire.TDO.driven := true.B
-          j.TCK := jtag_wire.TCK
-          j.TMS := jtag_wire.TMS
-          j.TDI := jtag_wire.TDI
-
-          val io_jtag = Wire(new JTAGPins(() => new BasePin(), false)).suggestName("jtag")
-
-          JTAGPinsFromPort(io_jtag, jtag_wire)
-
-          io_jtag.TCK.i.ival := IBUFG(IOBUF(th.jd_2).asClock).asBool
-
-          IOBUF(th.jd_5, io_jtag.TMS)
-          PULLUP(th.jd_5)
-
-          IOBUF(th.jd_4, io_jtag.TDI)
-          PULLUP(th.jd_4)
-
-          IOBUF(th.jd_0, io_jtag.TDO)
-
-          // mimic putting a pullup on this line (part of reset vote)
-          th.SRST_n := IOBUF(th.jd_6)
-          PULLUP(th.jd_6)
-
-          // ignore the po input
-          io_jtag.TCK.i.po.map(_ := DontCare)
-          io_jtag.TDI.i.po.map(_ := DontCare)
-          io_jtag.TMS.i.po.map(_ := DontCare)
-          io_jtag.TDO.i.po.map(_ := DontCare)
+          // val jtag_wire = Wire(new JTAGIO)
+          // jtag_wire.TDO.data := j.TDO
+          // jtag_wire.TDO.driven := true.B
+          // j.TCK := jtag_wire.TCK
+          // j.TMS := jtag_wire.TMS
+          // j.TDI := jtag_wire.TDI
+          //
+          // val io_jtag = Wire(new JTAGPins(() => new BasePin(), false)).suggestName("jtag")
+          //
+          // JTAGPinsFromPort(io_jtag, jtag_wire)
+          //
+          // io_jtag.TCK.i.ival := IBUFG(GTP_IOBUF(th.jd_2).asClock).asBool
+          //
+          // GTP_IOBUF(th.jd_5, io_jtag.TMS)
+          // PULLUP(th.jd_5)
+          //
+          // GTP_IOBUF(th.jd_4, io_jtag.TDI)
+          // PULLUP(th.jd_4)
+          //
+          // GTP_IOBUF(th.jd_0, io_jtag.TDO)
+          //
+          // // mimic putting a pullup on this line (part of reset vote)
+          // th.SRST_n := GTP_IOBUF(th.jd_6)
+          // PULLUP(th.jd_6)
+          //
+          // // ignore the po input
+          // io_jtag.TCK.i.po.map(_ := DontCare)
+          // io_jtag.TDI.i.po.map(_ := DontCare)
+          // io_jtag.TMS.i.po.map(_ := DontCare)
+          // io_jtag.TDO.i.po.map(_ := DontCare)
         }
     }
   }
@@ -71,9 +71,9 @@ class WithPGL22GJTAGHarnessBinder extends OverrideHarnessBinder({
 
 class WithPGL22GUARTHarnessBinder extends OverrideHarnessBinder({
   (system: HasPeripheryUARTModuleImp, th: PGL22GFPGATestHarness, ports: Seq[UARTPortIO]) => {
-    withClockAndReset(th.clock_32MHz, th.ck_rst) {
-      IOBUF(th.uart_rxd_out,  ports.head.txd)
-      ports.head.rxd := IOBUF(th.uart_txd_in)
+    withClockAndReset(th.clock_32MHz, th.resetP) {
+      GTP_IOBUF(th.uart_rxd_out,  ports.head.txd)
+      ports.head.rxd := GTP_IOBUF(th.uart_txd_in)
     }
   }
 })
--- fpga/src/main/scala/pgl22g/TestHarness.scala
+++ fpga/src/main/scala/pgl22g/TestHarness.scala
@@ -6,7 +6,7 @@ import freechips.rocketchip.config.Parameters
 import chipyard.{BuildTop, HasHarnessSignalReferences}
 import chipyard.harness.ApplyHarnessBinders
 import chipyard.iobinders.HasIOBinders
-import sifive.fpgashells.shell.pangogshell.PGL22GShell
+import sifive.fpgashells.shell.pangogshell.{NegativeResetWrapper, PGL22GShell}
 
 class PGL22GFPGATestHarness(override implicit val p: Parameters) extends PGL22GShell with HasHarnessSignalReferences {
 
@@ -14,17 +14,20 @@ class PGL22GFPGATestHarness(override implicit val p: Parameters) extends PGL22GS
 
   // Convert harness resets from Bool to Reset type.
   val hReset = Wire(Reset())
-  hReset := ck_rst
+  hReset := resetP
 
   val dReset = Wire(AsyncReset())
   dReset := reset_core.asAsyncReset
 
+  val clockUse = CLK50MHZ
+
   // default to 32MHz clock
-  withClockAndReset(clock_32MHz, hReset) {
+  withClockAndReset(clockUse, hReset) {
+    // val dut = Module(new NegativeResetWrapper(lazyDut.module, moduleName = "ChipTopWrapper"))
     val dut = Module(lazyDut.module)
   }
 
-  val buildtopClock = clock_32MHz
+  val buildtopClock = clockUse
   val buildtopReset = hReset
   val success = false.B
 
