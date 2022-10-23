77f2e0c6ae24d8203ee0e7eca532a4d463f17369
Chiro Liang <Chiro2001@163.com>
2022-10-23 21:24:41 +0800

    port of picorv

--- fpga/src/main/scala/pgl22g/Configs.scala
+++ fpga/src/main/scala/pgl22g/Configs.scala
@@ -374,6 +374,28 @@ class PGL22GSodorUcodeConfig extends Config(
     new PGL22GSodorConfigBase
 )
 
+class PGL22GPicoRVConfig extends Config(
+  new picorv.WithNPicoRVCores(1) ++
+    new WithPGL22GTweaks ++
+    new WithPGL22GAXIMem(base = BigInt(0x80000000L)) ++
+    // new testchipip.WithSerialTLWidth(32) ++
+    // new testchipip.WithSerialPBusMem ++
+    // new WithSerialTLMem ++
+    // new WithTLIOPassthrough ++
+    // new freechips.rocketchip.subsystem.WithScratchpadsOnly ++ // use sodor tile-internal scratchpad
+    // new WithSmallScratchpadsOnly ++
+    // new freechips.rocketchip.subsystem.WithNoMemPort ++ // use no external memory
+    new WithoutFPU ++
+    new WithL2TLBs(0) ++
+    new WithL1ICacheSets(64 * 2) ++
+    new WithL1DCacheSets(64 * 2) ++
+    // new WithDefaultMemPort ++
+    // new freechips.rocketchip.subsystem.WithNBanks(0) ++
+    // new testchipip.WithRingSystemBus ++
+    new WithNMemoryChannels(1) ++
+    new WithBufferlessBroadcastHub ++
+    new ModifiedAbstractConfig)
+
 class SimTinyRocketPGL22GConfig extends Config(
   new WithPGL22GSimTweaks ++
     new PGL22GRocketConfig
