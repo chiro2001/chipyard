ab7f9c6aa86daca6a131f1d93cb57b201e5bc53d
Chiro Liang <Chiro2001@163.com>
2022-11-05 14:08:46 +0800

    start jump to CLINT

--- software/tests/main.c
+++ software/tests/main.c
@@ -1,9 +1,14 @@
 #include "firmware.h"
 #include "sifive-uart.h"
 
+#define CLIT_ADDR 0x2000000
+
 void main(void) {
   uart_init();
   hello();
+  print_str("jumping to CLINT...\n");
+  (*(void (*)(void))(*((uint32_t*)(0x2000000))))();
+  print_str("exec done\n");
 }
 
 void park(void) {
--- software/tests/print.c
+++ software/tests/print.c
@@ -6,34 +6,31 @@
 // means.
 
 #include "firmware.h"
+#include "sifive-uart.h"
 
-void print_chr(char ch)
-{
-	*((volatile uint32_t*)UART_PORT) = ch;
+void print_chr(char ch) {
+  sifive_uart_putc(ch);
+  // *((volatile uint32_t *)UART_PORT) = ch;
 }
 
-void print_str(const char *p)
-{
-	while (*p != 0)
-		*((volatile uint32_t*)UART_PORT) = *(p++);
+void print_str(const char *p) {
+  while (*p != 0)
+    print_chr(*(p++));
 }
 
-void print_dec(unsigned int val)
-{
-	char buffer[10];
-	char *p = buffer;
-	while (val || p == buffer) {
-		*(p++) = val % 10;
-		val = val / 10;
-	}
-	while (p != buffer) {
-		*((volatile uint32_t*)UART_PORT) = '0' + *(--p);
-	}
+void print_dec(unsigned int val) {
+  char buffer[10];
+  char *p = buffer;
+  while (val || p == buffer) {
+    *(p++) = val % 10;
+    val = val / 10;
+  }
+  while (p != buffer) {
+    print_chr('0' + *(--p));
+  }
 }
 
-void print_hex(unsigned int val, int digits)
-{
-	for (int i = (4*digits)-4; i >= 0; i -= 4)
-		*((volatile uint32_t*)UART_PORT) = "0123456789ABCDEF"[(val >> i) % 16];
+void print_hex(unsigned int val, int digits) {
+  for (int i = (4 * digits) - 4; i >= 0; i -= 4)
+    print_chr("0123456789ABCDEF"[(val >> i) % 16]);
 }
-
