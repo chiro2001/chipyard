a256ee6040520499b584bd404aab7fa9e100c7f8
Chiro Liang <Chiro2001@163.com>
2022-11-09 04:35:59 +0800

    smp update

--- fpga/src/main/scala/pgl22g/simple/VexChipSmpWrapper.scala
+++ fpga/src/main/scala/pgl22g/simple/VexChipSmpWrapper.scala
@@ -2,9 +2,11 @@ package pgl22g.simple
 
 import chipsalliance.rocketchip.config.{Config, Field, Parameters}
 import chisel3._
-import freechips.rocketchip.amba.axi4.{AXI4MasterNode, AXI4MasterParameters, AXI4MasterPortParameters, AXI4SlaveNode, AXI4SlaveParameters, AXI4SlavePortParameters, AXI4Xbar}
+import freechips.rocketchip.amba.axi4.{AXI4Fragmenter, AXI4MasterNode, AXI4MasterParameters, AXI4MasterPortParameters, AXI4RAM, AXI4SlaveNode, AXI4SlaveParameters, AXI4SlavePortParameters, AXI4ToTL, AXI4UserYanker, AXI4Xbar}
 import freechips.rocketchip.diplomacy.{AddressSet, LazyModule, LazyModuleImp, LazyModuleImpLike, LazyRawModuleImp, TransferSizes}
+import freechips.rocketchip.tilelink.{TLBuffer, TLFIFOFixer, TLIdentityNode, TLWidthWidget}
 import pgl22g.DDR3Mem
+import sifive.blocks.devices.uart.{UART, UARTParams}
 import sifive.fpgashells.ip.pango.ddr3.PGL22GMIGIODDR
 import vexriscv.chipyard._
 import vexriscv.demo.VexOnChipConfig
@@ -32,15 +34,36 @@ class VexChipSmpWrapper(implicit p: Parameters) extends LazyModule {
         masters = Seq(AXI4MasterParameters("cpuDBus" + i)))))
     ))
   val xbar = AXI4Xbar()
-  val axiSlave = AXI4SlaveNode(Seq(AXI4SlavePortParameters(
+  val axiDeviceMem = AXI4SlaveNode(Seq(AXI4SlavePortParameters(
     slaves = Seq(AXI4SlaveParameters(
       address = Seq(AddressSet(0x80000000L, 0xfffffffL)),
       executable = true,
       supportsWrite = TransferSizes(1, 256 * 8),
       supportsRead = TransferSizes(1, 256 * 8))),
     beatBytes = 8)))
-  axiSlave := xbar
+  axiDeviceMem := xbar
+  val rom = AXI4RAM(
+    AddressSet(0x10000L, 0xffffL),
+    beatBytes = 8
+  )
+  rom := AXI4Fragmenter() := xbar
   axiMasters.foreach(master => xbar := master)
+
+  // val slaveNode = TLIdentityNode()
+  // val masterNode = TLIdentityNode()
+  // // xbar := AXI4Fragmenter() := AXI4UserYanker(Some(2)) := AXI4ToTL() := TLWidthWidget(4) := slaveNode
+  // (masterNode
+  //   := TLBuffer()
+  //   := TLFIFOFixer(TLFIFOFixer.all)
+  //   := TLWidthWidget(4)
+  //   := AXI4ToTL()
+  //   // := AXI4UserYanker(Some(2))
+  //   := AXI4Fragmenter()
+  //   := xbar)
+  // slaveNode := masterNode
+  //
+  // val uart = LazyModule(new UART(4, UARTParams(0x54000000L)))
+  // slaveNode := uart.node
 
   override def module = new VexChipSmpWrapperImpl(this)
 }
@@ -52,7 +75,7 @@ class VexChipSmpWrapperImpl(_outer: VexChipSmpWrapper) extends LazyRawModuleImp(
   val sys_clock = IO(Input(Clock()))
   val reset = IO(Input(Bool()))
 
-  val DDR3Mem = Module(new DDR3Mem(0x80000000L, _outer.axiSlave.in.head._1.params))
+  val DDR3Mem = Module(new DDR3Mem(0x80000000L, _outer.axiDeviceMem.in.head._1.params))
   val ddr = IO(new PGL22GMIGIODDR)
   ddr.connectPads(DDR3Mem.ddrIO)
   // mem.ddrIO.pad_loop_in := ddr.pad_loop_in
@@ -60,7 +83,7 @@ class VexChipSmpWrapperImpl(_outer: VexChipSmpWrapper) extends LazyRawModuleImp(
   val mainClock = DDR3Mem.ddrCtrl.pll_aclk_1
   val mainReset = WireInit(!DDR3Mem.ddrCtrl.pll_lock)
   DDR3Mem.ddrCtrl.aclk_1.get := mainClock
-  DDR3Mem.axi <> _outer.axiSlave.in.head._1
+  DDR3Mem.axi <> _outer.axiDeviceMem.in.head._1
   DDR3Mem.ddrCtrl.csysreq_ddrc := true.B
   DDR3Mem.ddrCtrl.csysreq_1 := true.B
   DDR3Mem.ddrCtrl.ddrc_rst := false.B
@@ -88,6 +111,27 @@ class VexChipSmpWrapperImpl(_outer: VexChipSmpWrapper) extends LazyRawModuleImp(
     x._1.io.connectIMem(x._2.head.out.head._1)
     x._1.io.connectDMem(x._2(1).out.head._1)
   })
+
+  def generateMemWatcher(address: Long, name: String = "resultReg"): UInt = {
+    val resultReg = RegInit(0.U(64.W)).suggestName(name)
+    val resultAddress = address.U
+    val validToResult = DDR3Mem.axi.aw.bits.addr === resultAddress && DDR3Mem.axi.aw.ready && DDR3Mem.axi.aw.valid
+    val readyToResult = RegInit(false.B)
+    val writtenResult = RegInit(false.B)
+    val writingResult = (validToResult || readyToResult) && DDR3Mem.axi.w.valid && DDR3Mem.axi.w.ready &&
+      DDR3Mem.axi.w.bits.last && writtenResult
+    when(validToResult) {
+      readyToResult := true.B
+    }
+    when(writingResult) {
+      writtenResult := true.B
+      resultReg := DDR3Mem.axi.w.bits.data
+    }
+    resultReg
+  }
+
+  val scores = IO(Output(UInt(64.W)))
+  scores := generateMemWatcher(0x85000000L, "scoresReg")
 }
 
 case object VexChipSmpKey extends Field[VexChipSmpWrapperConfig]
